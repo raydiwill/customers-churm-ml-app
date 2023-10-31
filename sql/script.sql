9f0c9e458492e4c2d87ac37f063440a69af44e53
MassiZek <49779093+MassiZek@users.noreply.github.com>
2023-10-31 18:18:24 +0100

    Your commit message here

---
+++ sql/script.sql
@@ -0,0 +1,30 @@
+-- Active: 1698770219335@@127.0.0.1@5432@dspproject
+
+-- Create table for the customer data 
+CREATE TABLE customer_data (
+    CustomerId integer PRIMARY KEY,
+    Surname text,
+    CreditScore integer,
+    Geography text,
+    Gender text,
+    Age integer,
+    Tenure integer,
+    Balance numeric,
+    NumOfProducts integer,
+    HasCrCard integer,
+    IsActiveMember integer,
+    EstimatedSalary numeric,
+    Exited integer,
+    Complain integer,
+    SatisfactionScore integer,
+    CardType text,
+    PointEarned integer
+);
+
+-- Create table for the model predection result 
+CREATE TABLE model_predictions (
+    PredictionId serial PRIMARY KEY,
+    CustomerId integer REFERENCES customer_data(CustomerId),
+    PredictionResult numeric,
+    PredictionDate timestamp
+);
