a72e229d32c9ee0eafc0929c202b1a012772d59d
raydiwill <duong.tranhn1102@gmail.com>
2024-02-09 10:30:43 +0100

    Added comments

--- api/config.py
+++ api/config.py
@@ -8,6 +8,21 @@ load_dotenv(dotenv_path=env_path)
 
 
 class Settings:
+    """
+    Configuration settings for the application, including project details
+    and database connection parameters, sourced from environment variables.
+
+    Attributes:
+    PROJECT_NAME (str): Name of the project.
+    PROJECT_VERSION (str): Current version of the project.
+    POSTGRES_USER (str): Username for the PostgreSQL database.
+    POSTGRES_PASSWORD (str): Password for the PostgreSQL database.
+    POSTGRES_SERVER (str): Hostname of the PostgreSQL server.
+    POSTGRES_PORT (str): Port number for the PostgreSQL server.
+    POSTGRES_DB (str): Name of the PostgreSQL database to use.
+    DATABASE_URL (str): Full database connection URL.
+    """
+
     PROJECT_NAME: str = "Job Board"
     PROJECT_VERSION: str = "1.0.0"
 
@@ -16,9 +31,10 @@ class Settings:
     POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
-    DATABASE_URL = (f"postgresql://"
-                    f"{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:"
-                    f"{POSTGRES_PORT}/{POSTGRES_DB}")
+    DATABASE_URL: str = (f"postgresql://"
+                         f"{POSTGRES_USER}:"
+                         f"{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:"
+                         f"{POSTGRES_PORT}/{POSTGRES_DB}")
 
 
 settings = Settings()
