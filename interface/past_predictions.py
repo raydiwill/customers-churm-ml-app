905b394a9c1fbaf3e1fb9d6ae3eba786119031c7
raydiwill <duong.tranhn1102@gmail.com>
2024-02-09 10:08:28 +0100

    Refractored past-prediction page

--- interface/past_predictions.py
+++ interface/past_predictions.py
@@ -1,22 +1,25 @@
-import streamlit as st
 from utils import get_past_predictions, display_past_predictions
+import streamlit as st
 import datetime
 
 
 # Create the past predictions page
-def past_predictions_page(api_url, csv_file):
+def past_predictions_page(api_url):
     st.subheader("View Past Predictions")
     start_date = st.date_input("Start Date")
     today = datetime.date.today()
     end_date = st.date_input("End Date",
                              max_value=today + datetime.timedelta(days=1))
 
-    prediction_source = st.selectbox("Prediction Source", ["webpage", "scheduled", "all"])
+    prediction_source = st.selectbox("Prediction Source",
+                                     ["webpage", "scheduled", "all"])
+
     data = {
         "start_date": start_date.strftime("%Y-%m-%d"),
         "end_date": end_date.strftime("%Y-%m-%d"),
-        "pred_source" : prediction_source
+        "pred_source": prediction_source
     }
+
     # Get past predictions from the CSV file
     predictions = get_past_predictions(api_url, data)
 
